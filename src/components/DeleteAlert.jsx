c8d46727c8f320e2e4b7991d49f8a4f01d404e13
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-05-11 21:27:12 +0600

    added delete button

--- src/components/DeleteAlert.jsx
+++ src/components/DeleteAlert.jsx
@@ -1,47 +1,63 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { TrashBin } from "@gravity-ui/icons";
 import { AlertDialog, Button } from "@heroui/react";
 import { redirect } from "next/navigation";
 
+const DeleteAlert = ({ destination }) => {
+    const { _id, destinationName } = destination;
+    
+    const handleDelete = async() => {
+      const res = await fetch(`http://localhost:5000/destination/${_id}`, {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(destination)
+        });
+        const data = await res.json()
+        redirect('/destinations')
+    }
 
-const DeleteAlert = () => {
-  const { _id, destinationName } = destination;
+    return (
+        <AlertDialog>
+            <Button className={"text-red-500 rounded-none"} variant="outline">
+                <TrashBin /> Delete
+            </Button>
+            <AlertDialog.Backdrop>
+                <AlertDialog.Container>
+                    <AlertDialog.Dialog className="sm:max-w-[400px]">
+                        <AlertDialog.CloseTrigger />
+                        <AlertDialog.Header>
+                            <AlertDialog.Icon status="danger" />
+                            <AlertDialog.Heading>
+                                Delete destination permanently?
+                            </AlertDialog.Heading>
+                        </AlertDialog.Header>
+                        <AlertDialog.Body>
+                            <p>
+                                This will permanently delete{" "}
+                                <strong>{destinationName}</strong> and all of
+                                its data. This action cannot be undone.
+                            </p>
+                        </AlertDialog.Body>
+                        <AlertDialog.Footer>
+                            <Button slot="close" variant="tertiary">
+                                Cancel
+                            </Button>
+                            <Button
+                                onClick={handleDelete}
+                                slot="close"
+                                variant="danger"
+                            >
+                                Delete
+                            </Button>
+                        </AlertDialog.Footer>
+                    </AlertDialog.Dialog>
+                </AlertDialog.Container>
+            </AlertDialog.Backdrop>
+        </AlertDialog>
+    );
+};
 
-  return (
-    <AlertDialog>
-      <Button className={"text-red-500 rounded-none"} variant="outline">
-        <TrashBin /> Delete
-      </Button>
-      <AlertDialog.Backdrop>
-        <AlertDialog.Container>
-          <AlertDialog.Dialog className="sm:max-w-[400px]">
-            <AlertDialog.CloseTrigger />
-            <AlertDialog.Header>
-              <AlertDialog.Icon status="danger" />
-              <AlertDialog.Heading>
-                Delete destination permanently?
-              </AlertDialog.Heading>
-            </AlertDialog.Header>
-            <AlertDialog.Body>
-              <p>
-                This will permanently delete <strong>{destinationName}</strong>{" "}
-                and all of its data. This action cannot be undone.
-              </p>
-            </AlertDialog.Body>
-            <AlertDialog.Footer>
-              <Button slot="close" variant="tertiary">
-                Cancel
-              </Button>
-              <Button onClick={handleDelete} slot="close" variant="danger">
-                Delete
-              </Button>
-            </AlertDialog.Footer>
-          </AlertDialog.Dialog>
-        </AlertDialog.Container>
-      </AlertDialog.Backdrop>
-    </AlertDialog>
-  )
-}
-
-export default DeleteAlert
+export default DeleteAlert;
